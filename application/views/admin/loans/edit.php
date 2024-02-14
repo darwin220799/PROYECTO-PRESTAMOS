80f3f3dbca1cdd67145ca58b9e1edbc22a1f15b7
darwin220799 <m_durands@hotmail.com>
2024-02-14 17:40:09 -0500

    cambios 2 darwin

--- application/views/admin/loans/edit.php
+++ application/views/admin/loans/edit.php
@@ -51,8 +51,8 @@
     </div>
     <div class="form-row">
       <div class="form-group col-12 col-md-12">
-        <label class="small mb-1">Asesor de grupo</label>
-        <input class="form-control" id="user_name" type="text" readonly>
+        <label class="small mb-1">Asesor</label>
+        <input class="form-control" id="user_name" type="text" >
       </div>
     </div>
     <div class="form-row">
@@ -62,26 +62,8 @@
       </div>
       <div class="form-group col-12 col-md-4">
         <label class="small mb-1" for="in_amount">Tasa de interés %</label>
-        <select class="form-control" type="number" id="in_amount" name="interest_amount">
-        <option value="3">3%</option>
-        <option value="4">4%</option>
-        <option value="5">5%</option>
-        <option value="6">6%</option>
-        <option value="7">7%</option>
-        <option value="8">8%</option>
-        <option value="9">9%</option>
-          <option value="10">10%</option>
-          <option value="11">11%</option>
-          <option value="12">12%</option>
-          <option value="13">13%</option>
-          <option value="14" selected>14%</option>
-          <option value="15">15%</option>
-          <option value="16">16%</option>
-          <option value="17">17%</option>
-          <option value="18">18%</option>
-          <option value="19">19%</option>
-          <option value="20">20%</option>
-        </select>
+        <input class="form-control" type="number" id="in_amount" name="interest_amount">
+        
       </div>
       <div class="form-group col-12 col-md-4">
         <label class="small mb-1" for="payment_m">Modalidad de pago</label>
